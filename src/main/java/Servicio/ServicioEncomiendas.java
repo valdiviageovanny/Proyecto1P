a3e424ebdd108f05d53da47c701b228974b0e742
valdi <valdi@Geova>
2023-11-25 13:21:12 -0500

    Primer Commit

---
+++ src/main/java/Servicio/ServicioEncomiendas.java
@@ -0,0 +1,14 @@
+package Servicio;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author valdi
+ */
+public class ServicioEncomiendas {
+    
+}
